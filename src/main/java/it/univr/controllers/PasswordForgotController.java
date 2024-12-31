8fb0877576a04debca81f38c171823e92c7320cb
mattx00 <58653523+mattx00@users.noreply.github.com>
2024-12-31 14:56:36 +0100

    Create PasswordForgotController.java

---
+++ src/main/java/it/univr/controllers/PasswordForgotController.java
@@ -0,0 +1,45 @@
+package it.univr.controller;
+
+import it.univr.model.User;
+import it.univr.service.EmailService;
+import it.univr.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PasswordForgotController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private UserService userService;
+
+    public PasswordForgotController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/passwordForgot")
+    public String showPasswordForgotPage() {
+        return "PasswordForgotPage";
+    }
+
+    @PostMapping("/passwordForgot")
+    public String resetPasswordForgot(@RequestParam String email, Model model) {
+        User user = userService.findByEmail(email);
+
+        if (user != null) {
+            model.addAttribute("success", "Ciao '" + user.getUsername() + "'.\n\nSono state mandate le istruzioni a: " + email);
+            emailService.sendPasswordResetEmail(user.getEmail());
+        }
+        else {
+            model.addAttribute("error", "Indirizzo email non valido.");
+        }
+
+        return "PasswordForgotPage";
+    }
+}
