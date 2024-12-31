f930ca06fa43e3804edea0d2124821b102fac888
mattx00 <58653523+mattx00@users.noreply.github.com>
2024-12-31 14:41:22 +0100

    Update Project.java

--- src/main/java/it/univr/models/Project.java
+++ src/main/java/it/univr/models/Project.java
@@ -1,4 +1,43 @@
 package it.univr.models;
 
+import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Entity
 public class Project {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY) 
+  private Long id; 
+  private String name; 
+  private String description; 
+  @DateTimeFormat(pattern = "yyyy-MM-dd")
+  private LocalDate startDate; 
+  @DateTimeFormat(pattern = "yyyy-MM-dd")
+  private LocalDate endDate;
+
+  // Getters and Setters
+  
+  public long getId() {return id;}
+
+  public void setId(long id) {this.id = id;}
+
+  public String getName() {return name;}
+
+  public void setName(String name) {this.name = name;}
+
+  public String getDescription() {return description;}
+
+  public void setDescription(String description) {this.description = description;}
+
+  public LocalDate getStartDate() {return startDate;}
+
+  public void setStartDate(LocalDate startDate) {this.startDate = startDate;}
+
+  public LocalDate getEndDate() {return endDate;}
+
+  public void setEndDate(LocalDate endDate) {this.endDate = endDate;}
+  
 }
