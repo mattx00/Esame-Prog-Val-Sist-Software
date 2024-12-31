5f2914414e14fee17c752b5269ebe5ca4eb3b479
mattx00 <58653523+mattx00@users.noreply.github.com>
2024-12-31 14:53:59 +0100

    Create ErrorController.java

---
+++ src/main/java/it/univr/controllers/ErrorController.java
@@ -0,0 +1,13 @@
+package it.univr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorController {
+
+    @GetMapping("/error")
+    public String showErrorPage() {
+        return "ErrorPage";
+    }
+}
