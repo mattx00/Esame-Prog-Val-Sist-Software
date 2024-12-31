29b032597079a6488814c71071ec025077f99e88
mattx00 <58653523+mattx00@users.noreply.github.com>
2024-12-31 15:02:17 +0100

    Create PasswordResetTokenService.java

---
+++ src/main/java/it/univr/services/PasswordResetTokenService.java
@@ -0,0 +1,42 @@
+package it.univr.services;
+
+import it.univr.model.PasswordResetToken;
+import it.univr.model.User;
+import it.univr.repository.PasswordResetTokenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class PasswordResetTokenService {
+
+    @Autowired
+    private PasswordResetTokenRepository passwordResetTokenRepository;
+
+    public List<PasswordResetToken> findAll() {
+        return passwordResetTokenRepository.findAll();
+    }
+
+    public PasswordResetToken getANewToken(User user) {
+        PasswordResetToken token;
+
+        if (user.getToken() != null) {
+            token = user.getToken();
+        }
+        else {
+            token = new PasswordResetToken();
+        }
+
+        token.setToken(UUID.randomUUID().toString());
+        token.setExpiryDate(LocalDateTime.now().plusSeconds(30));
+        token.setUserUsername(user.getUsername());
+        return token;
+    }
+
+    public PasswordResetToken findByStringToken(String token) {
+        return passwordResetTokenRepository.findByToken(token);
+    }
+}
