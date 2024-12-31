1f108bf5b6c2a99e7d47d48ee21dab4f5c6a880e
mattx00 <58653523+mattx00@users.noreply.github.com>
2024-12-31 14:57:01 +0100

    Create DashboardPage.java

---
+++ src/main/java/it/univr/controllers/DashboardPage.java
@@ -0,0 +1,27 @@
+package it.univr.controller;
+
+import it.univr.model.User;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/dashboard")
+    public String showDashboardPage(HttpSession session, Model model) {
+        User user = (User) session.getAttribute("user");
+
+        model.addAttribute("userRole", user.getRole());
+        model.addAttribute("username", user.getUsername());
+
+        return "DashboardPage";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.removeAttribute("user");
+        return "LoginPage";
+    }
+}
