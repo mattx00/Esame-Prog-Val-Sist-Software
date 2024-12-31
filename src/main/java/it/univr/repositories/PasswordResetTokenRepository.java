c0fabea17fdb362e6cc9da59476e5516cc23c659
mattx00 <58653523+mattx00@users.noreply.github.com>
2024-12-31 14:59:53 +0100

    Create PasswordResetTokenRepository.java

---
+++ src/main/java/it/univr/repositories/PasswordResetTokenRepository.java
@@ -0,0 +1,8 @@
+package it.univr.repositories;
+
+import it.univr.model.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    PasswordResetToken findByToken(String token);
+}
